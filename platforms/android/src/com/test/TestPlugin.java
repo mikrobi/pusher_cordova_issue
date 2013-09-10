dbc33eca4658bb1c94ebfbdfc95904acbb73a9c0
Jakob Class <jakob.class@googlemail.com>
2013-09-10 12:59:33 +0200

    Call callback from same thread.

--- platforms/android/src/com/test/TestPlugin.java
+++ platforms/android/src/com/test/TestPlugin.java
@@ -23,11 +23,14 @@ public class TestPlugin extends CordovaPlugin {
 
     private void test(final CallbackContext callbackContext) {
         Log.d("TestPlugin", "executing test");
+        /*
         cordova.getThreadPool().execute(new Runnable() {
             @Override
             public void run() {
                 callbackContext.success("0cc63f47cc68c3c2283a");
             }
         });
+        */
+        callbackContext.success("0cc63f47cc68c3c2283a");
     }
 }
